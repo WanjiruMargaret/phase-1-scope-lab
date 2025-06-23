ccff0b2de423ec92478e18603bbcfb1ed74e749a
Your Name <maggiesamwells@gmail.com>
2025-06-23 11:14:08 +0300

    Test Passed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+const leastFavoriteCustomer = 'someone';
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else'; // should throw an error
+}
